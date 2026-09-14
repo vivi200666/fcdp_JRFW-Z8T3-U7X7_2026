e95c1e70788fa312dc2e8b50be9735623733cbf2
vivi200666 <v.m.j.r.vivis@gmail.com>
2026-09-14 10:26:27 -0500

    Ejericio numeros primos

---
+++ FP_PD_01_TALLER_REPASO_C/cb_prime_num_01.c
@@ -0,0 +1,49 @@
+/**
+ * @file cb_prime_num_01.c
+ * @brief Generar un arreglo de numeros aleatorios y contar cuantos son primos.
+ * @author Viviana Jimenez
+ * @date 11-09-2026
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 10
+void count(int *arr){
+    
+    int sum=0;
+    for(int i=0; i<N; i++){
+        int counter= 0;
+        for (int j=1; j<=arr[i];j++){
+            if(arr[i]%j==0){
+                counter++;
+            }
+        }
+        if (counter==2){
+            sum+=1;
+        }  
+    }
+    printf(" Los numeros primos que hay en el arreglo son %d", sum);
+}
+
+int main(){
+    srand(time(NULL));
+    printf("Arreglo");
+    
+    int *arr=(int*) malloc(N*sizeof(int));
+    printf("[");
+    for (int i=0; i<N; i++){
+        arr[i]=rand() % 100+1;
+        if (i==N-1){
+            printf("%d", arr[i]);
+        }else{
+            printf("%d, ", arr[i]);
+        }
+        
+    }
+    printf("]");
+    count(arr);
+    free(arr);
+    return 0;
+}
+
+
