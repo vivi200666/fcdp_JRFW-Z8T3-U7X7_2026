4a7f2903e4a2aafdcf28af52cf15b4e8191bc216
vivi200666 <v.m.j.r.vivis@gmail.com>
2026-09-19 12:45:59 -0500

    Ejercicios primos

---
+++ FD_PD_02_TALLER_DE_AMDAHL/primes_number_parallel.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <omp.h>
+#define N 50000
+// Function to check if number is prine
+int isPrime(int n) {
+    if (n < 2)
+        return 0;
+    for (int i = 2; i <=sqrt(n); i++) {
+        if (n % i == 0)
+            return 0;
+    }
+    return 1;
+}
+int main() {
+    double startTime = omp_get_wtime();
+    int counterPrimes = 0;
+
+    // Parallelize loop for search primes
+    #pragma omp parallel for reduction(+:counterPrimes)
+    for (int i = 2; i <= N; i++) {
+        if (isPrime(i))
+            counterPrimes++;
+    }
+
+    double elapsedTime = omp_get_wtime() - startTime;
+
+    printf("Primes found: %d\n", counterPrimes);
+    printf("Parallel Time: %.3fsg\n", elapsedTime);
+    printf("Parallel Time: %.3fsg\n", elapsedTime);
+}
+
+// Execution: OMP_NUM_THREADS=6 ./primes_number_parallel
